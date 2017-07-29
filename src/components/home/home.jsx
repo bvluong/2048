ea348cf858e30e93b2103e53617db8be111b4dc4
bvluong <bryantvinluong@gmail.com>
2017-07-28 20:37:41 -0700

    Add createRow function to generate each box

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,32 +2,56 @@ import React from 'react';
 
 class Home extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      block01: 2,
+      block02: 0,
+      block03: 0,
+      block04: 0,
+      block11: 0,
+      block12: 2,
+      block13: 0,
+      block14: 0,
+      block21: 2,
+      block22: 4,
+      block23: 0,
+      block24: 0,
+      block31: 0,
+      block32: 0,
+      block33: 0,
+      block34: 0
+    };
+    this.createRow.bind(this);
+  }
+
+  createRow(rowNum) {
+    const row = [];
+    for (var i = 1; i < 5; i++) {
+      let blockID = `block${rowNum}${i.toString()}`;
+
+      row.push(<div className="block ${rowNum}${i.toString()}">
+        {this.state[blockID] === 0 ? "" : this.state[blockID]}
+      </div>);
+    }
+
+    return row;
+  }
+
   render () {
     return (
     <div className='home'>
       <div className="row row-0">
-        <div className="block 0-0"></div>
-        <div className="block 0-1"></div>
-        <div className="block 0-2"></div>
-        <div className="block 0-3"></div>
+        {this.createRow("0")}
       </div>
       <div className="row row-1">
-        <div className="block 1-0"></div>
-        <div className="block 1-1"></div>
-        <div className="block 1-2"></div>
-        <div className="block 1-3"></div>
+        {this.createRow("1")}
       </div>
       <div className="row row-2">
-        <div className="block 2-0"></div>
-        <div className="block 2-1"></div>
-        <div className="block 2-2"></div>
-        <div className="block 2-3"></div>
+        {this.createRow("2")}
       </div>
       <div className="row row-3">
-        <div className="block 3-0"></div>
-        <div className="block 3-1"></div>
-        <div className="block 3-2"></div>
-        <div className="block 3-3"></div>
+        {this.createRow("3")}
       </div>
     </div>
     );
