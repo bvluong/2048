616ec4df59edb17f700270067e2c50b95111cd4a
bvluong <bryantvinluong@gmail.com>
2017-07-28 20:08:36 -0700

    Add div blocks for each square

---
+++ src/components/home/home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Home extends React.Component {
+
+  render () {
+    return (
+    <div className='home'>
+      <div className="row row-0">
+        <div className="block 0-0"></div>
+        <div className="block 0-1"></div>
+        <div className="block 0-2"></div>
+        <div className="block 0-3"></div>
+      </div>
+      <div className="row row-1">
+        <div className="block 1-0"></div>
+        <div className="block 1-1"></div>
+        <div className="block 1-2"></div>
+        <div className="block 1-3"></div>
+      </div>
+      <div className="row row-2">
+        <div className="block 2-0"></div>
+        <div className="block 2-1"></div>
+        <div className="block 2-2"></div>
+        <div className="block 2-3"></div>
+      </div>
+      <div className="row row-3">
+        <div className="block 3-0"></div>
+        <div className="block 3-1"></div>
+        <div className="block 3-2"></div>
+        <div className="block 3-3"></div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Home;
